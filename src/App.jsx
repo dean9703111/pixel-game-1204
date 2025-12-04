8cefc998ce260ee2c9b845d2f2591df48b280b7f
dean <babydragon9703111@gmail.com>
2025-12-04 21:39:38 +0800

    fix: 將路由從 `BrowserRouter` 改為 `HashRouter`

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import GamePage from './pages/GamePage';
 import ResultPage from './pages/ResultPage';
